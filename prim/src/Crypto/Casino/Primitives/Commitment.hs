759b9f0b5bd167e98acbb109fcc51e6b8591d102
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-06-07 15:21:35 +0100

    add new commitment capabilities
    add key parts for coin flipping protocol, and generation

--- prim/src/Crypto/Casino/Primitives/Commitment.hs
+++ prim/src/Crypto/Casino/Primitives/Commitment.hs
@@ -18,19 +18,28 @@ module Crypto.Casino.Primitives.Commitment
     where
 
 import           Basement.Nat
-import           Foundation.List.ListN (ListN)
-import qualified Foundation.List.ListN as ListN
+import           Basement.Sized.List (ListN)
+import qualified Basement.Sized.List as ListN
 import           Foundation.Check
 import           Data.Proxy
+import           Data.Type.Equality
+import           Unsafe.Coerce (unsafeCoerce)
 import           GHC.Generics
 import           Control.DeepSeq
 import           Crypto.Random
 import           Crypto.Casino.Primitives.SSize
+import           Crypto.Casino.Primitives.HelperListN (rewriteListN_P1M1)
 import           Crypto.Casino.Primitives.ECC
+import qualified Crypto.Casino.Primitives.DLOG as DLOG
 import           Crypto.Casino.Primitives.TEG (Random)
 import           Crypto.Casino.Primitives.Matrix
 
 type H = Point
+
+data KeyPart = KeyPart
+    { keyPartPoint :: Point
+    , keyPartProof :: DLOG.Proof
+    } deriving (Show,Eq,Generic)
 
 data CommitmentKey (n :: Nat) = CommitmentKey
     { commitmentsKeyGs :: ListN n Point
@@ -53,11 +62,41 @@ instance (KnownNat n) => Show (Commitment (n :: Nat)) where
 instance (KnownNat n, NatWithinBound Int n) => Arbitrary (CommitmentKey n) where
     arbitrary = arbitrary >>= \drg -> pure $ fst (withDRG (drgNewTest drg) keyNew)
 
+keyPartGenerate :: MonadRandom randomly => randomly KeyPart
+keyPartGenerate = do
+    w <- keyGenerate
+    k <- keyGenerate
+    let point = pointFromSecret k
+    let proof = DLOG.generate w k (DLOG.DLOG curveGenerator point)
+    pure $ KeyPart point proof
+
+keyPartCombine :: forall n . ListN (n+1) KeyPart -> Maybe (CommitmentKey n)
+keyPartCombine l
+    | proofsValid = Just $ CommitmentKey
+        { commitmentsKeyGs = rewriteListN_P1M1 $ ListN.map keyPartPoint gs
+        , commitmentsKeyH  = keyPartPoint $ h
+        }
+    | otherwise = Nothing
+  where
+    proofsValid = and $ ListN.unListN $ ListN.map verifyOneProof l
+    verifyOneProof keyPart = DLOG.verify (DLOG.DLOG curveGenerator $ keyPartPoint keyPart) (keyPartProof keyPart)
+    (h, gs) = case x_lessequal_n_plus_x @1 @n of
+                Refl -> (ListN.head l, ListN.tail l)
+
+x_lessequal_n_plus_x :: forall x n . ('True :~: (x <=? (n + x)))
+x_lessequal_n_plus_x = unsafeCoerce Refl
+
 keyNew :: forall n randomly . ( MonadRandom randomly, KnownNat n, NatWithinBound Int n)
        => randomly (CommitmentKey n)
 keyNew = CommitmentKey <$> ListN.replicateM pointGenerate
                        <*> pointGenerate
   where pointGenerate = pointFromSecret <$> keyGenerate
+
+keyAdd :: CommitmentKey n -> CommitmentKey n -> CommitmentKey n
+keyAdd ck1 ck2 = CommitmentKey
+    { commitmentsKeyGs = ListN.zipWith (.+) (commitmentsKeyGs ck1) (commitmentsKeyGs ck2)
+    , commitmentsKeyH  = commitmentsKeyH ck1 .+ commitmentsKeyH ck2
+    }
 
 -- | Compute COM_ck( {a1, ... , an} , r )
 commitment :: CommitmentKey n -> ListN n Scalar -> Random -> Commitment n
